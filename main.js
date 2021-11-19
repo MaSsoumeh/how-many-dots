ffbd5e30e4a871b867b6077111697a2c2f025e2a
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-20 01:39:24 +0330

    Add extra check for ی

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ document.querySelector('.btn').addEventListener('click', () => {
   //Your Code
   let counter = 0;
   const inputValue = document.querySelector('.input').value;
+  console.log(inputValue.length);
   for (let i = 0; i < inputValue.length; i++) {
     const letter = inputValue.charAt(i);
     for (const [key, value] of Object.entries(obj)) {
       if (key === letter) {
-        counter += +value;
+        if (letter === 'ی') {
+          if (inputValue.charAt(i + 1) !== ' ' && i !== inputValue.length - 1) {
+            counter += 2;
+          }
+        } else {
+          counter += +value;
+        }
       } else if (value === letter) {
         counter += +key;
       }
